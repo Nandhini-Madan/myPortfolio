d1438023e53dcd26acae91b62ab4fe2df335e689
unknown <nandhinimadan20@gmail.com>
2021-03-01 10:22:16 -0800

    Project Base Layout completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import react from 'react';
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hi</h1>
-        <p>hello bro</p>
-      </header>
+  <div className="App">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-3">
+          <div className="app_sidebar">
+            <Sidebar/>
+          </div>
+        </div>
+        <div className="col-lg-9 app_main">col6</div>
+      </div>
     </div>
+  </div>
   );
 }
 
