d97629c8d9333581caa754858b9028a5bb475cd9
unknown <nandhinimadan20@gmail.com>
2021-03-01 22:16:04 -0800

    Sidebar components started

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,9 +3,29 @@ import React from 'react'
 
 const Sidebar=()=>{
     return(
-        <h1>
-            Sidebar
-        </h1>
+        <div>
+            <img src="" alt="" className="sidebar_image"/>
+            <div className="sidebar_name">Nandhini<span> Devi</span></div>
+            <div className="sidebar_item sidebar title">Web Development</div> 
+            <a href="">
+                <div className="sidebar_item">
+                    <img src="" alt="" className="sidebar_icon"
+                    div/> 
+                </div>
+            </a>
+            <figure className="sidebar_social_icons">
+                <a href=""><img src="" alt="facebook" className="sidebar_icon"/></a>
+                <a href=""><img src="" alt="instagram" className="sidebar_icon"/></a>
+            </figure>
+            <div className="contact">
+                <div className="sidebar_item">
+                    <a href="" ><img src="" alt="github"/>github</a>
+                </div>
+            <div className="sidebar_item">nandhini.madan20@gmail.com</div>
+            
+            </div>
+            
+    </div>
     )
 }
 
