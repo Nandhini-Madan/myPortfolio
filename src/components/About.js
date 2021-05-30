81d519bc3400bd7e6a575e92fbad90ab2ed500a2
unknown <nandhinimadan20@gmail.com>
2021-05-29 22:44:24 -0700

    Updated About Me pages with icons for skills

--- src/components/About.js
+++ src/components/About.js
@@ -4,30 +4,50 @@ import {Link} from "react-router-dom";
 import { Button,Card } from 'antd';
 import "../components/About.css";
 import 'antd/dist/antd.css';
+import {useHistory} from 'react-router-dom';
+import reactPic from '../assets/physics.png';
 
 const { Title ,Paragraph} = Typography;
 const { Meta } = Card;
+ 
 
 const About=()=>{
+  let history=useHistory();
+  const projectHandler=() => {
+    history.push({
+      pathname: '/Projects',
+      
+    });
+  };
+  const resumeHandler=()=>{
+    history.push({
+      pathname:'/Resume'
+    })
+  }
 return(
    <div >
       <Title className="tittle">About Me</Title>
       <Paragraph>Hi, I am a Full Stack Web Developer with hands-on experience in designing, developing, and implementing applications and solutions using a range of technologies and programming languages.
          Want to know how I may help your project? Check out my <Link to="/Project">project</Link> portfolio and <Link to="/Resume">online resume</Link>.</Paragraph>
-<br></br>
-<br></br>
-<Space>
-<Button type="primary">Project</Button>
-<Button type="primary">View Resume</Button>
+
+<div className="Primary_button">
+<Space size={"large"}>
+<Button type="primary" onClick={projectHandler}>Project</Button>
+<Button type="primary" onClick={resumeHandler}>View Resume</Button>
 </Space>
-
-
+</div>
 <br></br>
 <br></br>
 <hr></hr>
 <br></br>
 <Title level={2} className="tittle">Skills</Title>
 <Paragraph></Paragraph>
+  <div className="icon_container">
+    <img className="icons" src={reactPic} alt=""/>
+    <img className="icons" src={reactPic}/>
+    <img className="icons" src={reactPic}/>
+    <img className="icons" src={reactPic}/>  
+  </div>
 <br></br>
 <br></br>
 <hr></hr>
