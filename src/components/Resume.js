5ad4b68cd6fab75de5675ea81e7bef8326999ac3
unknown <nandhinimadan20@gmail.com>
2021-05-30 23:51:55 -0700

    Created Resume file and download function for resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import {Row, Col} from 'antd';
+import { Typography } from 'antd';
+import portfolio from '../assets/Nandhini-FullStackDeveloper.pdf';
 
+const { Title } = Typography;
 const Resume=()=>{
 return(
-   <div >Resume1</div>
+   <div >
+      <a href={portfolio} download>
+
+         <div>Download</div>
+      </a>
+      <div>
+      <Row>
+      <Col span={16}>
+      
+      <Title level={2}>Nandhini Devi Madan</Title>
+      <Title level={4}>Full Stack Web developer</Title>
+<hr></hr>
+      </Col>
+      
+      <Col span={8}>
+         <div>
+            hjkhjhj
+            nkm,mlm
+         </div>
+      </Col>
+    </Row>
+      </div>
+   </div>
 )
 }
 
